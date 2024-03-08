8a02ea3fa8593b84feb835ab384983a35098f074
queralt-chan <queraltbali@gmail.com>
2024-03-08 15:33:22 +0800

    connect to server

--- web/main.js
+++ web/main.js
@@ -4,6 +4,31 @@ const htmlList = document.querySelector ('.js_list');
 
 let listBooks = [];
 
+async function loadData () {
+
+    const response = await fetch ('http://localhost:5000/api/libros');
+    const dataLibros = await response.json();
+    listBooks = dataLibros;
+    renderLibros();
+}
+
+loadData();
+
+
 function renderLibros () {
     let listBooks = '';
+    for (book of listBooks) {
+        html += renderOne(book);
+    }
+    htmlList.innerHTML = listBooks;
 }
+
+function renderOne (libro) {
+    const { nombre, autor, paginas} = libro;
+    return 
+    `<li class=".js_list">
+    <h3 class="name">${nombre}</h3>
+    <h3 class="author">${autor}</h3>
+    <h3 class="pages"> ${paginas}</h3>
+    </li>`
+};
